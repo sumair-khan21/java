8cda2311850f02e2c826340d555390a69cf4e564
sumair-khan21 <johnalex21210@gmail.com>
2022-11-06 16:10:17 +0500

    Work Complete

---
+++ app.js
@@ -0,0 +1,47 @@
+// var spec = prompt("Your species?", "human");
+// var numberOfCats = prompt("How many cats?");
+// var x = prompt("Where does the Pope live?");
+// var correctAnswer = "Vatican";
+//  if (x === correctAnswer) {
+//  score++;
+//  userIQ = "genius";
+//  alert("Correct!");
+//  }
+// if (yourTicketNumber !== 487208) {
+//      alert("Better luck next time.");
+//   }
+// var a=10;
+// var b=10;
+// if (a !== b){
+//     console.log("A is not equal to b");
+// }if (a < b){
+//     console.log("A is less b")
+// }
+// var x = prompt("Where does the Pope live?");
+//  if (x === "Vatican") {
+//  alert("Correct!");
+//  }
+// var x = prompt("Where does the Pope live?");
+//  if (x === "Vatican") {
+//  alert("Correct!"); }
+//  if (x !== "Vatican") {
+//  alert("Wrong answer");
+//  }
+// if (weight > 300 && time < 6) {
+//      alert("Come to our tryout!");
+//      }
+//      else {
+//      alert("Come to our cookout!");
+//      }
+if (c === d) {
+   if (x === y) {
+   g = h;
+   }
+   else if (a === b) {
+   g = h;   }   else {
+   e = f;
+       }
+       }
+       else {
+       e = f;
+       }
